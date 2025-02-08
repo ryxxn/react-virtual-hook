5c51bc1c1eb0819e4341fefd7580cb27b1e46b1f
박도륜 <88328436+ryxxn@users.noreply.github.com>
2025-02-08 23:31:55 +0900

    feat: implement useVirtualized hook for optimized virtual scrolling

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,42 @@
+"use strict";
+var __create = Object.create;
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var src_exports = {};
+__export(src_exports, {
+  useVirtualList: () => use_virtual_list_default
+});
+module.exports = __toCommonJS(src_exports);
+
 // src/use-virtual-list.ts
-import React from "react";
-import { flushSync } from "react-dom";
+var import_react = __toESM(require("react"));
+var import_react_dom = require("react-dom");
 var SCROLLING_WAIT = 100;
 var useVirtualized = ({
   data,
@@ -8,15 +44,15 @@ var useVirtualized = ({
   rowHeight,
   buffer = 50
 }) => {
-  const [scrollTop, setScrollTop] = React.useState(0);
-  const [scrollLeft, setScrollLeft] = React.useState(0);
-  const [isScrollingX, setIsScrollingX] = React.useState(false);
-  const [isScrollingY, setIsScrollingY] = React.useState(false);
-  const containerRef = React.useRef(null);
-  const scrollTimeoutXRef = React.useRef(null);
-  const scrollTimeoutYRef = React.useRef(null);
+  const [scrollTop, setScrollTop] = import_react.default.useState(0);
+  const [scrollLeft, setScrollLeft] = import_react.default.useState(0);
+  const [isScrollingX, setIsScrollingX] = import_react.default.useState(false);
+  const [isScrollingY, setIsScrollingY] = import_react.default.useState(false);
+  const containerRef = import_react.default.useRef(null);
+  const scrollTimeoutXRef = import_react.default.useRef(null);
+  const scrollTimeoutYRef = import_react.default.useRef(null);
   const scrollXHandler = (newScrollLeft) => {
-    flushSync(() => {
+    (0, import_react_dom.flushSync)(() => {
       setIsScrollingX(true);
       setScrollLeft(newScrollLeft);
     });
@@ -28,7 +64,7 @@ var useVirtualized = ({
     );
   };
   const scrollYHandler = (newScrollTop) => {
-    flushSync(() => {
+    (0, import_react_dom.flushSync)(() => {
       setIsScrollingY(true);
       setScrollTop(newScrollTop);
     });
@@ -53,7 +89,7 @@ var useVirtualized = ({
         scrollYHandler(newScrollTop);
     });
   };
-  React.useEffect(() => {
+  import_react.default.useEffect(() => {
     const container = containerRef.current;
     if (!container)
       return;
@@ -77,15 +113,15 @@ var useVirtualized = ({
   );
   const visibleData = data.slice(startIdx, endIdx);
   const totalHeight = data.length * rowHeight;
-  const getContainerStyle = React.useCallback(
+  const getContainerStyle = import_react.default.useCallback(
     () => ({ maxHeight: height, height }),
     [height]
   );
-  const getListStyle = React.useCallback(
+  const getListStyle = import_react.default.useCallback(
     () => ({ height: totalHeight, position: "relative" }),
     [totalHeight]
   );
-  const getRowStyle = React.useCallback(
+  const getRowStyle = import_react.default.useCallback(
     (index) => ({
       width: "100%",
       height: rowHeight,
@@ -111,6 +147,7 @@ var useVirtualized = ({
   };
 };
 var use_virtual_list_default = useVirtualized;
-export {
-  use_virtual_list_default as useVirtualList
-};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  useVirtualList
+});
